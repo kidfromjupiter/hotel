867638f0cba64df04b797c3deb35f0db9aeeeae8
lasan <limahaliyana@gmail.com>
2026-09-19 21:19:06 +0530

    added dependency injection to report routes

--- backend/app/api/dependencies.py
+++ backend/app/api/dependencies.py
@@ -1,11 +1,17 @@
 from app.repositories.booking_flow_repo import BookingFlowRepository
 from app.services.booking_flow_service import BookingFlowService
 from app.services.otp_service import OTPService
+from app.services.report_service import ReportService
 
 # TODO: Make these stateless. That means removing the singleton pattern
+report_service = ReportService()
 booking_flow_repo = BookingFlowRepository()
 booking_flow_service = BookingFlowService(booking_flow_repo=booking_flow_repo)
 otp_service = OTPService()
+
+
+def get_report_service() -> ReportService:
+    return report_service
 
 
 def get_booking_flow_repo() -> BookingFlowRepository:
